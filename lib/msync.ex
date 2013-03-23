d637dcac35060deafd20f9b1d09ded775e9a0e7e
Nirvana Platform <hello@nirvana.io>
2013-03-23 19:28:05 -0300

    now working.

--- lib/msync.ex
+++ lib/msync.ex
@@ -5,17 +5,22 @@ defmodule Msync do
 		dbid = :d
 		Couchie.open(dbid)
 		IO.puts "Getting list of files...."
-		flist = Path.wildcard("./elixir/*")
-		IO.puts "Uploading files:"
+		File.cd("./elixir")
+		flist = Path.wildcard("*")
+		IO.puts "Uploading files: #{flist}"
 		Enum.each flist, upload_file(dbid, &1)
     # Enum.map paths, :unicode.characters_to_binary(&1)
 
 
 	end
 
-	defp upload_file(dbid, file) do
-		IO.puts "Opening file #{file}"
-		File.open file
+	defp upload_file(dbid, path) do
+		IO.puts "Opening file #{path}"
+		{:ok, binary} = File.read path
+		IO.puts "Got #{binary} \n\n*****\n\n"
+		dict = HashDict.new [{:code, binary}]
+		IO.puts "Storing #{path} to database"
+		Couchie.set(dbid, path, dict)
 	end
 
 end

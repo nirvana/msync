0e455f30f365ec5e85585e622b3b56f4391f0d6f
Nirvana Platform <hello@nirvana.io>
2013-03-25 16:46:12 -0300

    Support other hosts/buckets. Language and type fields.

--- lib/msync.ex
+++ lib/msync.ex
@@ -1,10 +1,14 @@
 defmodule Msync do
 
-	def run do
-		#IO.puts "opening connection to the db"
+	def run() do
+		run('localhost:8091', '', '')  #default bucket
+	end
+
+	def run(host, bucket, password) do
+		IO.puts "Opening connection to the db"
 		dbid = :d
-		Couchie.open(dbid)
-		#IO.puts "Getting list of files...."
+		Couchie.open(dbid, 10, host, bucket, password)
+		IO.puts "Getting list of files...."
 		File.cd("./elixir")
 		flist = Path.wildcard("*")
 		#IO.puts "Uploading files: #{flist}"
@@ -15,11 +19,11 @@ defmodule Msync do
 	end
 
 	defp upload_file(dbid, path) do
-		#IO.puts "Opening file #{path}"
+		IO.puts "Opening file #{path}"
 		{:ok, binary} = File.read path
 		#IO.puts "Got #{binary} \n\n*****\n\n"
-		dict = HashDict.new [{:code, binary}]
-		#IO.puts "Storing #{path} to database"
+		dict = HashDict.new [{:type, "code"},{:language, "elixir"},{:code, binary}]
+		IO.puts "Storing #{path} to database"
 		Couchie.set(dbid, path, dict)
 	end
 

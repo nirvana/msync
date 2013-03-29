7bfc73dbf96e4f5d7a99ae6df807e680a214d16e
Nirvana Platform <hello@nirvana.io>
2013-03-29 16:08:49 -0300

    re-added gitignore. updated dependency pointers in mix

--- mix.exs
+++ mix.exs
@@ -15,6 +15,6 @@ defmodule Msync.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [{:couchie, github: "NirvanaPlatform/couchie"}]
+    [{:couchie, github: "nirvana/couchie"}]
   end
 end
